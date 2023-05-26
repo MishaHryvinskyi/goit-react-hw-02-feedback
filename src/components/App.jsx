9ae3a7c0f273d222959ec6d761b11ddcfccf4807
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-05-26 20:25:57 +0300

    joke

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      Тут могла б бути Ваша реклама! Але буде моя 2га домашка!)
     </div>
   );
 };
