cc12f12d22905b0f597a50bf557dd29c6b93d2d0
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-05-27 01:10:25 +0300

    c

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,99 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class Statistics extends Component {
-  static defaultProps = {
-    initialValue: 0,
-  };
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+  <div>
+    <p>
+      Good: <span>{good}</span>
+    </p>
+    <p>
+      Neutral: <span>{neutral}</span>
+    </p>
+    <p>
+      Bad: <span>{bad}</span>
+    </p>
+    <p>
+      Total Feedback: <span>{total}</span>
+    </p>
+    <p>
+      Positive Feedback Percentage: <span>{positivePercentage}%</span>
+    </p>
+  </div>
+);
 
-  state = {
-    feedback: {
-      good: 0,
-      neutral: 0,
-      bad: 0
-    }
-  };
-
-  handleGoodClick = () => {
-    this.setState((prevState) => ({
-      feedback: {
-        ...prevState.feedback,
-        good: prevState.feedback.good + 1
-      }
-    }));
-  };
-  
-  handleNeutralClick = () => {
-    this.setState((prevState) => ({
-      feedback: {
-        ...prevState.feedback,
-        neutral: prevState.feedback.neutral + 1
-      }
-    }));
-  };
-  
-  handleBadClick = () => {
-    this.setState((prevState) => ({
-      feedback: {
-        ...prevState.feedback,
-        bad: prevState.feedback.bad + 1
-      }
-    }));
-  };
-
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state.feedback;
-    return good + neutral + bad;
-  }
-
-  countPositiveFeedbackPercentage() {
-    const { good } = this.state.feedback;
-    const totalFeedback = this.countTotalFeedback();
-    return totalFeedback === 0 ? 0 : Math.round((good / totalFeedback) * 100);
-  }
-
-  render() {
-    const { feedback } = this.state;
-    const totalFeedback = this.countTotalFeedback();
-    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
-
-    return (
-      <div>
-        <section>
-          <h2>Please leave feedback</h2>
-          <div>
-            <button onClick={this.handleGoodClick}>Good</button>
-            <button onClick={this.handleNeutralClick}>Neutral</button>
-            <button onClick={this.handleBadClick}>Bad</button>
-          </div>
-        </section>
-        <section>
-          <h2>Statistics</h2>
-          {totalFeedback > 0 ? (
-            <div>
-              <p>
-                Good: <span>{feedback.good}</span>
-              </p>
-              <p>
-                Neutral: <span>{feedback.neutral}</span>
-              </p>
-              <p>
-                Bad: <span>{feedback.bad}</span>
-              </p>
-              <p>
-                Total Feedback: <span>{totalFeedback}</span>
-              </p>
-              <p>
-                Positive Feedback Percentage:{" "}
-                <span>{positiveFeedbackPercentage}%</span>
-              </p>
-            </div>
-          ) : (
-            <p>No feedback given yet.</p>
-          )}
-        </section>
-      </div>
-    );
-  }
-}
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
 
 export default Statistics;
